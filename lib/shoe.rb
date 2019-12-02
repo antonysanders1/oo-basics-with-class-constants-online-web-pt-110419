5db01f176c4b0244200bd87acd3cfd3200c3cd01
Antony Sanders <antony.devon92@gmail.com>
2019-12-02 07:06:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    # I'm thinking I should iterate through all brands 1st, delete any duplicate brand i.e "Nike", then push the remaining brands to the Constant Array
+    BRANDS << brand
+    
+  
   end
+  
 
   def cobble
     self.condition = "new"
